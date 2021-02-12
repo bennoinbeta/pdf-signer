2cb1a2027d5c48cf3a7f4cdce571860116a2e0be
BennoDev <bennokohrs@gmail.com>
2021-02-12 12:09:58 +0100

    added combinePages 'feature'

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { readFilesFromDir, writePdf } from "./file";
 const pdfOutputPath = "./files/output/";
 const pdfInputPath = "./files/input/";
 const signPngImageInputPath = "./files/signs/";
+const combineIntoOnePdf = true;
+const combinedPdfName = "final";
 
 const start = async (): Promise<void> => {
   // Read Pdfs
@@ -14,15 +16,18 @@ const start = async (): Promise<void> => {
   const signsImages: Uint8Array[] = [];
   for (const key in signData) signsImages.push(signData[key]);
 
+  const combinedPdfDoc = await PDFDocument.create();
+
   // Embed Signs and Save File
   for (const filename in pdfData) {
     console.log("Info: Start Editing File", filename);
     const pdfDoc = await PDFDocument.load(pdfData[filename]);
+    const pages = pdfDoc.getPages();
     const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
-    const pages = pdfDoc.getPages();
-
-    for (const page of pages) {
+    for (let i = 0; i < pages.length; i++) {
+      console.log("Info: Start Editing Page", i);
+      const page = pages[i];
       const { width, height } = page.getSize();
       const randomSignImageIndex = Math.floor(
         Math.random() * signsImages.length
@@ -49,12 +54,32 @@ const start = async (): Promise<void> => {
         color: rgb(0.95, 0.1, 0.1),
         rotate: degrees(-45),
       });
+      console.log("Info: End Editing Page ", i);
     }
     console.log("Info: End Editing File", filename);
 
+    // Add Page to combined Pdf Doc
+    if (combineIntoOnePdf) {
+      const copiedPages = await combinedPdfDoc.copyPages(
+        pdfDoc,
+        pdfDoc.getPageIndices()
+      );
+      copiedPages.forEach((page) => {
+        combinedPdfDoc.addPage(page);
+      });
+    }
+
     // Save PDF
-    const pdfBytes = await pdfDoc.save();
-    await writePdf(filename, pdfOutputPath, pdfBytes);
+    if (!combineIntoOnePdf) {
+      const pdfBytes = await pdfDoc.save();
+      await writePdf(filename, pdfOutputPath, pdfBytes);
+    }
+  }
+
+  // Save combinedPDF
+  if (combineIntoOnePdf) {
+    const pdfBytes = await combinedPdfDoc.save();
+    await writePdf(combinedPdfName, pdfOutputPath, pdfBytes);
   }
 };
 
